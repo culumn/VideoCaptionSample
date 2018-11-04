8d6ac19d40fa29dccf2470f7ca0cdfc71047d545
culumn <yoxtsushi.44106@gmail.com>
2018-11-05 00:27:38 +0900

    Init xcode project

---
+++ VideoCaptionSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoCaptionSample
+//
+//  Created by Matsuoka Yoshiteru on 2018/11/05.
+//  Copyright © 2018 culumn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
